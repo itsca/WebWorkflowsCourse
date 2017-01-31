4574f5985b10794861b0f69202a1c2eb970fa49b
itsca <itscae@gmail.com>
2017-01-30 20:22:42 -0600

    Added CoffeeScript task and files, change components location

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,12 @@
 var gulp = require('gulp'),
-	gutil = require('gulp-util');
+	gutil = require('gulp-util'),
+	coffee = require('gulp-coffee');
 
-gulp.task('log', function() {
-	gutil.log("workflows are awsome");
+var coffeeSources = ['components/coffee/tagline.coffee']; //Array of locations of coffee script files.
+
+gulp.task('coffee', function() {
+	gulp.src('coffeeSources') //Location of the coffee script.
+		.pipe(coffee({bare : true}) //pipe it to coffee and safe wrappit.
+			.on('error', gutil.log)) //listen for erros and log them instead of stoping terminal.
+		.pipe(gulp.dest('components/scripts')) //pipe it to gulp to add the compile file to the scripts forlder.
 });
